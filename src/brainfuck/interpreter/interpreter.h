cda60e2a971ed2a10f9c5613ecf86695b89f66a0
llucere <66846791+llucere@users.noreply.github.com>
2022-01-20 22:20:48 +0200

    Update interpreter.h

--- src/brainfuck/interpreter/interpreter.h
+++ src/brainfuck/interpreter/interpreter.h
@@ -1,7 +1,5 @@
 #pragma once
 #define bfbreak goto _brainfuck_break;
 
-void bf_shl(uint16_t* cell, int repeats);
-void bf_shr(uint16_t* cell, int repeats);
 uint16_t* bf_optimize(Interpreter* interpret);
 int bf_interpreter(Interpreter* interpret);
