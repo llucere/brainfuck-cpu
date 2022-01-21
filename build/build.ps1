47d3e8951befe5c5152be4c5a3822cb192d5111d
llucere <66846791+llucere@users.noreply.github.com>
2022-01-21 15:51:27 +0200

    Update build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -1,7 +1,7 @@
-gcc ../src/main.c -o main -O2
+gcc ../src/main.c -o bfcpu -O2
 
 #Clear-Host
 Write-Output "START:"
 Write-Output "-----------------------------------------------------"
-./main.exe ../tests/multi_string_input.bf -bytecodepass -time
+./bfcpu.exe ../tests/multi_string_input.bf -bytecodepass -time
 Write-Output "-----------------------------------------------------"
